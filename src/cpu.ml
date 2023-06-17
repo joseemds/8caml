495711c71f9c04e3e9fcb663ff3b8964fb253948
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-06-17 16:32:42 -0300

    inital code

--- src/cpu.ml
+++ src/cpu.ml
@@ -1,44 +1,42 @@
-type register =
-  | V0
-  | V1
-  | V2
-  | V3
-  | V4
-  | V5
-  | V6
-  | V7
-  | V8
-  | V9
-  | VA
-  | VB
-  | VC
-  | VD
-  | VE
-  | VF
-  | SP
-  | PC
-  | DT
-  | ST
+module Register = struct
+  type t =
+    | V0
+    | V1
+    | V2
+    | V3
+    | V4
+    | V5
+    | V6
+    | V7
+    | V8
+    | V9
+    | VA
+    | VB
+    | VC
+    | VD
+    | VE
+    | VF
+    | SP
+    | PC
+    | DT
+    | ST
 
-
-
-let hex_of_register = function
-  | V0 -> 0x0
-  | V1 -> 0x1
-  | V2 -> 0x2
-  | V3 -> 0x3
-  | V4 -> 0x4
-  | V5 -> 0x5
-  | V6 -> 0x6
-  | V7 -> 0x7
-  | V8 -> 0x8
-  | V9 -> 0x9
-  | VA -> 0xA
-  | VB -> 0xB
-  | VC -> 0xC
-  | VD -> 0xD
-  | VE -> 0xE
-  | VF -> 0xF
-  | _ -> failwith("Not implemented")
-
-
+  let hex_of_register = function
+    | V0 -> 0x0
+    | V1 -> 0x1
+    | V2 -> 0x2
+    | V3 -> 0x3
+    | V4 -> 0x4
+    | V5 -> 0x5
+    | V6 -> 0x6
+    | V7 -> 0x7
+    | V8 -> 0x8
+    | V9 -> 0x9
+    | VA -> 0xA
+    | VB -> 0xB
+    | VC -> 0xC
+    | VD -> 0xD
+    | VE -> 0xE
+    | VF -> 0xF
+    | _ -> failwith "Not implemented"
+end
--- src/instructions.ml
+++ src/instructions.ml
@@ -1,14 +1,32 @@
+module Nibble = struct
+  let first_nibble byte = byte lsr 12
+  let second_nibble byte = byte land (0XF00 lsr 8)
+  let third_nibble byte = byte land (0X0F0 lsr 4)
+  let last_nibble byte = byte land 0X0F0
+
+  let nibble_of_hex hex =
+    (first_nibble hex, second_nibble hex, third_nibble hex, last_nibble hex)
+end
+
 type t =
   | Clear
   | Return
-  | Jump(Bytes.t)
-  | Call(Bytes.t)
-  | SetRegister(int * int)
-  | ADD(int * int)
-  | SUB(int * int)
-  | LD(int * int)
-  | OR(int * int)
-  | AND(int * int)
-  | XOR(int * int)
-  | SkipIf(register * register)
+  | Add of int * int
+  | Sub of int * int
+  | Ld of int * int
+  | Or of int * int
+  | And of int * int
+  | Xor of int * int
+  | Jump of Bytes.t
+  | Call of Bytes.t
+  | SetRegister of int * int
+  | SkipIf of Cpu.Register.t * Cpu.Register.t
   | Invalid
+
+let instruction_of_hex hex =
+  let nibbles = Nibble.nibble_of_hex hex in
+  match nibbles with
+  | 0x0, 0x0, 0xE, 0x0 -> Clear
+  | 0x0, 0x0, 0xE, 0xE -> Return
+  (* | 0x1, n1, n2, n3 ->   *)
+  | _, _, _, _ -> Invalid
